01a201efe088e266f1029bafaede55384d415471
David Vinje <davidvinje@Davids-MacBook-Air.local>
2024-02-09 22:34:03 +0100

    day 2

---
+++ aoc-2021/python/day2.py
@@ -0,0 +1,27 @@
+lines = [(a, int(b)) for (a, b) in [x.split() for x in open('../input/day2.txt').readlines()]]
+
+def part_one():
+    depth, position = 0, 0
+    for direction, amount in lines:
+        if direction == 'forward':
+            position += amount
+        elif direction == 'down':
+            depth += amount
+        else:
+            depth -= amount
+    print(depth * position)
+
+def part_two():
+    aim, depth, position = 0, 0, 0
+    for direction, amount in lines:
+        if direction == 'forward':
+            position += amount
+            depth += aim * amount
+        elif direction == 'down':
+            aim += amount
+        else:
+            aim -= amount
+    print(depth * position)
+
+part_one()
+part_two()
