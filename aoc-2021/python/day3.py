198509a03c8fb5dbd130acbec5ad4bb910849c1c
David Vinje <dnvinje@gmail.com>
2024-02-13 13:41:33 +0100

    day3 python

--- aoc-2021/python/day3.py
+++ aoc-2021/python/day3.py
@@ -19,4 +19,31 @@ def part_one():
     gamma, epsilon = int(gamma, 2), int(epsilon, 2)
     print(gamma * epsilon)
 
+def scrub(lst, is_oxygen):
+    i = 0
+    while len(lst) > 1:
+        count = {'0': 0, '1': 0}
+        for line in lst:
+            bit = line[i]
+            count[bit] += 1
+        if is_oxygen:
+            if count['0'] > count['1']:
+                lst = [x for x in lst if x[i] == '0']
+            else:
+                lst = [x for x in lst if x[i] == '1']
+        else:
+            if count['1'] < count['0']:
+                lst = [x for x in lst if x[i] == '1']
+            else:
+                lst = [x for x in lst if x[i] == '0']
+        i += 1
+    return lst[0]
+
+def part_two():
+    oxygen = scrub(lines, True)  
+    carbondioxide = scrub(lines, False)
+    carbondioxide, oxygen = int(carbondioxide, 2), int(oxygen, 2)
+    print(carbondioxide * oxygen)
+
 part_one()
+part_two()
