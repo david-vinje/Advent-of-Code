a710b56e1d3ace3d9c0467a88316c7a6ad0edf1d
David Vinje <dnvinje@gmail.com>
2024-02-26 15:53:22 +0100

    day1 f#

--- aoc-2021/f#/day1.fsx
+++ aoc-2021/f#/day1.fsx
@@ -1,1 +1,16 @@
-using I
+let measurements = 
+    System.IO.File.ReadAllLines("./input/day1.txt")
+    |> Array.map int
+
+let partOne arr = 
+    Array.pairwise arr
+    |> Array.fold (fun acc elem -> 
+        if snd elem > fst elem
+        then acc + 1 else acc) 0
+
+// partOne measurements
+
+let partTwo = 
+    Array.windowed 3 measurements
+    |> Array.map Array.sum
+    |> partOne
