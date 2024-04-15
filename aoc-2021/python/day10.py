8ff2d16e7e33ca5be97bd2c7c5640931bee33445
David Vinje <dnvinje@gmail.com>
2024-04-15 14:45:09 +0200

    day 10

---
+++ aoc-2021/python/day10.py
@@ -0,0 +1,60 @@
+lines = [list(x.rstrip()) for x in open('./input/day10.txt').readlines()]
+opening = {'[', '(', '{', '<'}
+pairs = {
+    ']': '[',
+    '}': '{',
+    ')': '(',
+    '>': '<'
+}
+def part_one():
+    total = 0
+    scoring = {
+        ')': 3,
+        ']': 57,
+        '}': 1197,
+        '>': 25137
+    }
+    for line in lines:
+        stack = []
+        for chunk in line:
+            if chunk in opening:
+                stack.append(chunk)
+            else:
+                opener = stack.pop()
+                if pairs[chunk] != opener:
+                    total += scoring[chunk]
+                    break
+    print(total)
+    
+def part_two():
+    scores = []
+    scoring = {
+        '(': 1,
+        '[': 2,
+        '{': 3,
+        '<': 4
+    }
+    for line in lines:
+        stack = []
+        corrupted = False
+        for chunk in line:
+            if chunk in opening:
+                stack.append(chunk)
+            else:
+                opener = stack.pop()
+                if pairs[chunk] != opener:
+                    corrupted = True
+                    break
+        if not corrupted:
+            score = 0
+            while stack:
+                chunk = stack.pop()
+                score *= 5
+                score += scoring[chunk]
+            scores.append(score)
+    scores.sort()
+    m = int(len(scores)/2)
+    print(scores[m])
+
+part_one()
+part_two()
