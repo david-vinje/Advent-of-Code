343165c21d5bb025cd689f81dc1f3775b6c30121
David Vinje <dnvinje@gmail.com>
2024-05-06 15:24:48 +0200

    2023 day 2 C#

--- aoc-2022/C#/Day2.cs
+++ aoc-2022/C#/Day2.cs
@@ -11,26 +11,6 @@ namespace Program
         {
             Rock = 1, Paper, Scissors
         }
-        List<(Hand, Hand)> ParseInput()
-        {
-            return File.ReadAllLines("../input/day2.txt")
-                .Select(line => line.Split())
-                .Select(line =>
-                {
-                    return (line[0], line[1]) switch
-                    {
-                        ("A", "X") => (Hand.Rock, Hand.Rock),
-                        ("A", "Y") => (Hand.Rock, Hand.Paper),
-                        ("A", _) => (Hand.Rock, Hand.Scissors),
-                        ("B", "X") => (Hand.Paper, Hand.Rock),
-                        ("B", "Y") => (Hand.Paper, Hand.Paper),
-                        ("B", _) => (Hand.Paper, Hand.Scissors),
-                        (_, "X") => (Hand.Scissors, Hand.Rock),
-                        (_, "Y") => (Hand.Scissors, Hand.Paper),
-                        _ => (Hand.Scissors, Hand.Scissors)
-                    };
-                }).ToList();
-        }
 
         int battle(Hand opponent, Hand me)
         {
@@ -48,10 +28,51 @@ namespace Program
             return score;
         }
 
-        void Part1(List<(Hand, Hand)> matches)
+        void Part1()
         {
-            var result = matches
-                .Aggregate(0, (acc, elem) =>
+            var result = File.ReadAllLines("../input/day2.txt")
+                .Select(line => line.Split())
+                .Select(line =>
+                {
+                    return (line[0], line[1]) switch
+                    {
+                        ("A", "X") => (Hand.Rock, Hand.Rock),
+                        ("A", "Y") => (Hand.Rock, Hand.Paper),
+                        ("A", _) => (Hand.Rock, Hand.Scissors),
+                        ("B", "X") => (Hand.Paper, Hand.Rock),
+                        ("B", "Y") => (Hand.Paper, Hand.Paper),
+                        ("B", _) => (Hand.Paper, Hand.Scissors),
+                        (_, "X") => (Hand.Scissors, Hand.Rock),
+                        (_, "Y") => (Hand.Scissors, Hand.Paper),
+                        _ => (Hand.Scissors, Hand.Scissors)
+                    };
+                }).Aggregate(0, (acc, elem) =>
+                {
+                    var score = battle(elem.Item1, elem.Item2);
+                    return acc + score;
+                });
+            Console.WriteLine(result);
+        }
+
+        void Part2()
+        {
+            var result = File.ReadAllLines("../input/day2.txt")
+                .Select(line => line.Split())
+                .Select(line =>
+                {
+                    return (line[0], line[1]) switch
+                    {
+                        ("A", "X") => (Hand.Rock, Hand.Scissors),
+                        ("A", "Y") => (Hand.Rock, Hand.Rock),
+                        ("A", _) => (Hand.Rock, Hand.Paper),
+                        ("B", "X") => (Hand.Paper, Hand.Rock),
+                        ("B", "Y") => (Hand.Paper, Hand.Paper),
+                        ("B", _) => (Hand.Paper, Hand.Scissors),
+                        (_, "X") => (Hand.Scissors, Hand.Paper),
+                        (_, "Y") => (Hand.Scissors, Hand.Scissors),
+                        _ => (Hand.Scissors, Hand.Rock)
+                    };
+                }).Aggregate(0, (acc, elem) =>
                 {
                     var score = battle(elem.Item1, elem.Item2);
                     return acc + score;
@@ -61,8 +82,8 @@ namespace Program
 
         public Day2()
         {
-            var matches = ParseInput();
-            Part1(matches);
+            // Part1();
+            Part2();
         }
     }
 }
