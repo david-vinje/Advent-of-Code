d3333a24008243a6adade4f3e894530001c7ace8
david-vinje <dnvinje@gmail.com>
2024-12-03 07:24:47 +0100

    2024 day 3 python

---
+++ aoc-2024/python/day3.py
@@ -0,0 +1,17 @@
+import re
+
+lines = open('./input/day3.txt')
+total = 0
+enabled = True
+for line in lines:
+  matches = re.findall("(mul\([0-9]*,[0-9]*\)|don't|do?)", line)
+  for match in matches:  
+    if match == "don't":
+      enabled = False
+    elif match == 'do':
+      enabled = True
+    elif enabled:
+      mults = [int(x) for x in match.lstrip("mul(").rstrip(")").split(",")]
+      total += mults[0] * mults[1]
+      print(mults, total)
+print(total)
