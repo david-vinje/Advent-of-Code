b90ba03330e28cd3196215d7cc277d733656a4b4
david-vinje <dnvinje@gmail.com>
2024-12-11 08:40:09 +0100

    m

--- aoc-2024/python/day11.py
+++ aoc-2024/python/day11.py
@@ -1,15 +1,10 @@
-# stones = [int(x) for x in open('./input/day11.txt').read().split()]
+stones = open('./input/day11.txt').read().split()
 
-# 0 -> 1
-# 2n -> split
-# 2n-1 -> n*=2024
-
-def blink(stones: dict):
-  new_stones = {}
-  for stone, count in stones.items():
-    if int(stone) == 0:
-      new_stones.setdefault(stone, 0)
-      new_stones['1'] += count
+def blink(stones: list):
+  new_stones = []
+  for stone in stones:
+    if stone == '0':
+      new_stones.append('1')
     elif len(stone) % 2 == 0:
       m = int(len(stone) / 2)
       left, right = int(stone[:m]), int(stone[m:])
@@ -20,20 +15,21 @@ def blink(stones: dict):
       new_stones.append(str(stone))
   return new_stones
 
-def part_one(n):
-  for i in range(n+1):
-    stones = open('./input/day11.txt').read().split()
-    for _ in range(i):
-      # print(' '.join(stones))
-      stones = blink(stones)
-    print(i, len(stones))
-# part_one(75)
+# stones = list(range(1, 10))
+# for i in range(1, 10):
+#   stone = str(i * 2024)
+#   m = int(len(stone) / 2)
+#   left, right = stone[:m], stone[m:]
+#   m = int(len(left) / 2)
+#   a, b = left[:m], left[m:]
+#   c, d = right[:m], right[m:]
+#   print(i, ':', [a, b, c, d])
 
-def part_two():
-  stones = {}
-  for stone in open('./input/day11.txt').read().split():
-    stones.setdefault(stone, 0)
-    stones[stone] += 1
-  print(stones)
-
-part_two()
+# for stone in ['20', '44', '68', '92', '16']:
+#   m = int(len(stone) / 2)
+#   left, right = stone[:m], stone[m:]
+#   print([left, right])
+  
+for i in range(25):
+  stones = blink(stones)
+print(len(stones))
