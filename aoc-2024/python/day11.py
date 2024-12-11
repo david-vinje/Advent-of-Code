3c854ddebcdd5848de65c3c76aa2a44f407650f0
david-vinje <dnvinje@gmail.com>
2024-12-11 08:13:56 +0100

    make dict

--- aoc-2024/python/day11.py
+++ aoc-2024/python/day11.py
@@ -1,15 +1,15 @@
-stones = open('./input/day11.txt').read().split()
 # stones = [int(x) for x in open('./input/day11.txt').read().split()]
 
 # 0 -> 1
 # 2n -> split
 # 2n-1 -> n*=2024
 
-def blink(stones: list):
-  new_stones = []
-  for stone in stones:
+def blink(stones: dict):
+  new_stones = {}
+  for stone, count in stones.items():
     if int(stone) == 0:
-      new_stones.append('1')
+      new_stones.setdefault(stone, 0)
+      new_stones['1'] += count
     elif len(stone) % 2 == 0:
       m = int(len(stone) / 2)
       left, right = int(stone[:m]), int(stone[m:])
@@ -20,7 +20,20 @@ def blink(stones: list):
       new_stones.append(str(stone))
   return new_stones
 
-for i in range(75):
-  stones = blink(stones)
-res = ' '.join(stones) 
-print(res) 
+def part_one(n):
+  for i in range(n+1):
+    stones = open('./input/day11.txt').read().split()
+    for _ in range(i):
+      # print(' '.join(stones))
+      stones = blink(stones)
+    print(i, len(stones))
+# part_one(75)
+
+def part_two():
+  stones = {}
+  for stone in open('./input/day11.txt').read().split():
+    stones.setdefault(stone, 0)
+    stones[stone] += 1
+  print(stones)
+
+part_two()
